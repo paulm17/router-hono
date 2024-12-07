74d6ffa2bc9b6564ff9c2024211cce05990566b9
Paul Moss <paulmoss06@gmail.com>
2024-12-07 11:07:15 +0000

    build: another commit

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,11 +2,19 @@ import { betterAuth } from 'better-auth';
 import { customSessionClient } from 'better-auth/client/plugins';
 import { organization } from 'better-auth/plugins';
 import { createAuthClient } from 'better-auth/react';
-import Database from 'better-sqlite3';
+import { LibsqlDialect } from "@libsql/kysely-libsql";
+ 
+const dialect = new LibsqlDialect({
+    url: "libsql://temp-peterrushkin.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzM1Njk1NTEsImlkIjoiOGRlNmNlMmEtM2I1YS00YTdiLWE0Y2MtM2UxZGIwYTU5NzM0In0.nzectluIYydaFbhQOs8jj95C3RJQS8SJMDoHVmP2bpjn3uFfvml84DRigwIb15xnWHhrP0jSkJmSzjSIJ407CQ",
+})
 
 export const auth = betterAuth({
   trustedOrigins: ['http://localhost:5173'],
-  database: new Database("./database.sqlite"),
+  database: {
+    dialect,
+    type: "sqlite"
+  },
   emailAndPassword: {
     enabled: true,
   },
